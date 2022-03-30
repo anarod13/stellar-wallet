6d13070171f570c44e49c6a694a6c463f9d1e827
anarod13 <anam.rodriguez13@gmail.com>
2022-03-29 22:34:46 -0300

    master

--- src/stellarMethods/loadKeyPair.js
+++
@@ -1,7 +0,0 @@
-import StellarSdk from 'stellar-sdk';
-
-export const loadKeyPair = (/** @type {string} */ secretKey) => {
-	const SourceKeys= StellarSdk.Keypair.fromSecret(secretKey);
-	const publicKey = SourceKeys.publicKey();
-	return publicKey;
-};
